8513c76e66949ea2d2c3e91706ce58b3dbba3218
unknown <james.hong@abliminal.com>
2021-08-28 14:13:46 -0400

    updated nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,41 +3,63 @@ import "./styles.css";
 import profilePhoto from "../../assets/profile.jpg";
 import styled from "styled-components";
 
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkOutlineIcon from "@material-ui/icons/WorkOutline";
+import CodeIcon from "@material-ui/icons/Code";
+import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
+
 const Navigation = () => {
     // about, education, experience, skills, projects, contact
 
     const FloatingMenu = styled.nav`
         background: transparent;
-        border: 2px solid #fff;
-        padding: 5px;
+        // border: 2px solid #222;
+        border-radius: 5px;
+        // padding: 5px;
         width: 110px;
-        height: 300px;
+        height: 250px;
         z-index: 100;
         position: fixed;
         bottom: 10vh;
-        left: 10vw;
+        left: 3vw;
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        font-family: "Raleway";
+        font-size: 0.9em;
+        font-weight: 400;
     `;
 
     const MenuLink = styled.a`
-        // font-size: 0.9em;
+        // margin: 0 0.5em;
+        // padding: 15px;
+        // background: #c9c9c9;
         text-align: center;
-        margin: 0 0.5em;
+        height: 100%;
         color: white;
         text-decoration: none;
+        display: flex;
+        // flex-direction: column;
+        // justify-content: center;
     `;
 
     return (
         <>
             <FloatingMenu>
-                <MenuLink href="#about">About</MenuLink>
-                <MenuLink href="#education">Education</MenuLink>
-                <MenuLink href="#experience">Experience</MenuLink>
-                <MenuLink href="#skills">Skills</MenuLink>
-                <MenuLink href="#projects">Projects</MenuLink>
-                <MenuLink href="#contact">Contact</MenuLink>
+                <MenuLink href="#about">
+                    <AccountCircleIcon />
+                    &nbsp;&nbsp;About
+                </MenuLink>
+                <MenuLink href="#experience">
+                    <WorkOutlineIcon /> &nbsp;&nbsp;Work
+                </MenuLink>
+                <MenuLink href="#skills">
+                    <CodeIcon /> &nbsp;&nbsp;Projects
+                </MenuLink>
+                <MenuLink href="#contact">
+                    <AlternateEmailIcon />
+                    &nbsp;&nbsp;Contact
+                </MenuLink>
             </FloatingMenu>
         </>
     );
