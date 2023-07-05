19d25a289c31ad12c7409415f32199d06ae740b8
James <jamesyj.hong@gmail.com>
2023-07-05 16:11:31 -0400

    Updates

--- src/components/common/badge/styles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const BadgeContainer = styled.div`
-  border-radius: 1em;
-  padding: 0.25em 0.75em;
-  background: var(--palette-5-4);
-  color: #fff;
-`;
--- src/helpers/navigation/index.tsx
+++
@@ -1,5 +0,0 @@
-const navigateToExternalLink = (destination: string): Window | null => window.open(destination, "_blank");
-
-const navigateToEmailLink = (emailLink: string): string => (window.location.href = emailLink);
-
-export { navigateToExternalLink, navigateToEmailLink };
